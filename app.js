76f2683913c3642ab2ba650fb99d0b4b4fe5dc8f
Gunnar Gantzel <gunnar@gantzel.no>
2025-09-08 16:11:03 +0200

    Add alert test to verify initializeApp method execution
    - Added alert() to test if initializeApp method is actually called
    - This will help identify if the method runs but console.log fails
    - Temporary debugging measure to isolate the issue

--- app.js
+++ app.js
@@ -20,6 +20,9 @@ class FraværApp {
     }
 
     async initializeApp() {
+        // Use alert to test if method is called
+        alert('initializeApp method called!');
+        
         console.log('🚀 initializeApp method called!');
         console.log('📱 App Version:', this.version);
         console.log('🔧 Git Commit:', this.gitCommit);
